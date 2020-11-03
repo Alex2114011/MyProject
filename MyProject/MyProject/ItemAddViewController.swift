fb2816f8c50452799ff8ac3ab9032e587a589475
Alex2114011 <Alex2114011@gmail.com>
2020-11-03 18:32:54 +0300

    Set UI TextView  + logic work

--- MyProject/MyProject/ItemAddViewController.swift
+++ MyProject/MyProject/ItemAddViewController.swift
@@ -11,17 +11,22 @@ protocol ItemAddDelegate: AnyObject {
     func addItem(with text: String, subText: String)
 }
 
-class ItemAddViewController: UIViewController {
+class ItemAddViewController: UIViewController, UITextViewDelegate {
     
     weak var delegate: ItemAddDelegate?
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var subTitleTextView: UITextView!
+    var placeholder = "Введите текст"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        setAddButton()
+        updateSaveButtonState()
+        subTitleTextView.delegate = self
     }
     
     
@@ -30,8 +35,11 @@ class ItemAddViewController: UIViewController {
         view.backgroundColor = .white
         self.title = "SecondVC"
         saveButton.layer.cornerRadius = 5.0
-        setAddButton()
-        updateSaveButtonState()
+        subTitleTextView.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1).cgColor
+        subTitleTextView.layer.borderWidth = 0.5
+        subTitleTextView.layer.cornerRadius = 5
+        subTitleTextView.text = placeholder
+        subTitleTextView.textColor = #colorLiteral(red: 0.7960080504, green: 0.7961236835, blue: 0.8038249612, alpha: 1)
     }
     
     func setAddButton() { //добавляем кнопку на навбар
@@ -40,10 +48,34 @@ class ItemAddViewController: UIViewController {
     
     
     @IBAction func tapSaveButton(_ sender: UIButton) {
+        if subTitleTextView.text == "Введите текст"{
+            subTitleTextView.text = nil
+        }
         self.delegate?.addItem(with: titleTextField.text ?? "", subText: subTitleTextView.text ?? "") // передаем значение на ItemListViewController
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
-  
+  //MARK: Add placeholder in TextView
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if subTitleTextView.textColor == #colorLiteral(red: 0.7960080504, green: 0.7961236835, blue: 0.8038249612, alpha: 1){
+            subTitleTextView.text = nil
+            subTitleTextView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if subTitleTextView.text.isEmpty{
+            subTitleTextView.text = "Введите текст"
+            subTitleTextView.textColor = #colorLiteral(red: 0.7960080504, green: 0.7961236835, blue: 0.8038249612, alpha: 1)
+            placeholder = ""
+        } else {
+            placeholder = subTitleTextView.text
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        placeholder = subTitleTextView.text
+    }
     
     //MARK: Change State button
     
--- MyProject/MyProject/ItemListTableViewCell.swift
+++ MyProject/MyProject/ItemListTableViewCell.swift
@@ -14,7 +14,8 @@ class ItemListTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        titleLabel.textColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        subTitleLabel.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
